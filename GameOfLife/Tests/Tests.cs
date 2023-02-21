b12e530752c7b7c76a9f42cc22ef7d80a0b18676
Dan Osborne <daniel.osborne@red-gate.com>
2023-02-21 12:52:07 +0000

    Fixed the test

--- GameOfLife/Tests/Tests.cs
+++ GameOfLife/Tests/Tests.cs
@@ -9,23 +9,21 @@ public class Tests
     [Test]
     public void GetNeighbours()
     {
-        var board = new Board
+        var board = new Board();
+        board.Cells = new List<Cell>
         {
-            Cells = new List<Cell>
+            new(board)
             {
-                new()
-                {
-                    Location = new Point(10, 10)
-                },
-                new()
-                {
-                    Location = new Point(9, 9)
-                }
+                Location = new Point(10, 10)
+            },
+            new(board)
+            {
+                Location = new Point(9, 9)
             }
         };
 
         var neighbours = board.GetNeighbours(new Point(10, 9));
-        
+
         Assert.That(neighbours.Count == 2);
     }
 }
